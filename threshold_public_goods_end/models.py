3602566326af49bec187a9949cdbf1b6a952b117
mcdaij <68068255+mcdaij@users.noreply.github.com>
2020-09-09 11:01:12 -0400

    Custom Function example pushed

--- threshold_public_goods_end/models.py
+++ threshold_public_goods_end/models.py
@@ -42,25 +42,6 @@ class Group(BaseGroup):
 
 
 class Player(BasePlayer):
-    def TreatmentVars(self):
-        if(self.session.config['synchronous_game']):
-            wrll=len(self.get_others_in_subsession())#in this case the min in waiting room is the number in session
-        else:
-            wrll=self.session.config['waiting_room_lowerlimit']
-        return dict(
-            threshold_high = self.session.config['threshold_high'],
-            threshold_low = self.session.config['threshold_low'],
-            value_high = self.session.config['value_high'],
-            value_low = self.session.config['value_low'],
-            total_rounds = self.session.config['total_rounds'],
-            group_size = self.session.config['group_size'],
-            waiting_room_lowerlimit=wrll,
-            simultaneous = self.session.config['simultaneous'],
-            base_tokens = self.session.config['base_tokens'],
-            increment = self.session.config['increment'],
-            decision_timer = self.session.config['decision_timer'],
-            participation_payment = self.session.config['participation_payment']
-            );
     birth_year = models.IntegerField(
         min=1900,
         max=2020,
--- threshold_public_goods_game/models.py
+++ threshold_public_goods_game/models.py
@@ -69,33 +69,11 @@ class Subsession(BaseSubsession):
 class Group(BaseGroup):
     pass
 
+class fu(BasePlayer):
+    def twoPlusTwo():
+        return 4
 
 class Player(BasePlayer):
-    def TreatmentVars(self):
-        # if(self.session.config['synchronous_game']):
-        #     N=0
-        #     for a in self.session.get_participants():
-        #         if a.vars.get('timed_out', None) == False:
-        #             if a.vars.get('groupmate_timed_out', False)==False:
-        #                 N+=1
-        #     wrll=N#in this case the min in waiting room is the number in session
-        # else:
-        wrll=self.session.config['waiting_room_lowerlimit']
-        return dict(
-            threshold_high = self.session.config['threshold_high'],
-            threshold_low = self.session.config['threshold_low'],
-            value_high = self.session.config['value_high'],
-            value_low = self.session.config['value_low'],
-            total_rounds = self.session.config['total_rounds'],
-            group_size = self.session.config['group_size'],
-            waiting_room_lowerlimit=wrll,
-            simultaneous = self.session.config['simultaneous'],
-            base_tokens = self.session.config['base_tokens'],
-            increment = self.session.config['increment'],
-            decision_timer = self.session.config['decision_timer'],
-            participation_payment = self.session.config['participation_payment']
-            );
-
     timed_out_round = models.IntegerField()
     #we don't have to worry about a maximum value here because 
     #the page will error if the total is too high and if we
