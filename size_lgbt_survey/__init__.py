7ab2f042ca0616430906d13ee0bffd9a34bad8f6
billuraksoy <44884690+billuraksoy@users.noreply.github.com>
2021-09-28 13:01:16 -0400

    lots of text edits

--- size_lgbt_survey/__init__.py
+++ size_lgbt_survey/__init__.py
@@ -114,7 +114,7 @@ class Player(BasePlayer):
     Hl = make_yn_field("Are you of Hispanic, Latino, or Spanish origin?")
     age = models.IntegerField(
       label = "What is your age in years?",
-      min=0,
+      min=18,
       max=150
       )
 
@@ -128,7 +128,7 @@ class Player(BasePlayer):
     marital = make_list_field("What is your marital status? (choose one)", ["Now married","Widowed","Divorced","Separated","Never married"])
 
     #page 2
-    occupants = models.IntegerField( label = "How many people live in your household including yourself", min=0, max=100)
+    occupants = models.IntegerField( label = "How many people live in your household including yourself?", min=0, max=100)
     children = models.IntegerField(label="How many children less than 18 years of age live in your household? If none, please put 0.\n \nNumber of children:", min= 0, max=100)
     education = make_list_field("What is the highest level of education you've completed? (choose one) (If currently enrolled, mark the previous grade or highest degree received.)", ["High school, GED, or less","Some college credits, no degree","Associate's degree (for example: AA, AS)","Bachelor’s degree or equivalent (for example: BA, BS)","Master’s degree (for example: MA, MS, MEng, MEd, MSW, MBA) or higher (for example: MD, DDS, DVM, LLB, JD, PhD, EdD)"])
     employment = make_list_field("Are you currently…?",["Employed for wages","Self-employed","Out of work for 1 year or more","Out of work for less than 1 year","A homemaker","A student","Retired","Unable to work"])
