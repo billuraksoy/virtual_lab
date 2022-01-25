be7b8db199d22af01eac4c7cf09bd6ab31f71a1f
Billur Aksoy <44884690+billuraksoy@users.noreply.github.com>
2022-01-25 13:53:53 -0500

    Update settings.py
    changed the completion link

--- settings.py
+++ settings.py
@@ -181,7 +181,7 @@ SESSION_CONFIGS = [
 
 SESSION_CONFIG_DEFAULTS = dict(
     #prolific
-    p_completion_link = 'https://app.prolific.co/submissions/complete?cc=745F08DE',
+    p_completion_link = 'https://app.prolific.co/submissions/complete?cc=5AC33EBF',
     #screenshot bot
     screenshot=False,
     use_browser_bots=False,
